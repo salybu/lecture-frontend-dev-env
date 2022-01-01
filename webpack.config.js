44d2f90e2280e27526951d97ecbe56092dd9d21f
salybu <rkdmsdl93@gmail.com>
2022-01-01 16:35:08 +0900

    ✅ loader 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        loader: "url-loader",
+        options: {
+          publicPath: "./dist/",
+          name: "[name].[ext]?[hash]",
+          limit: 20000, // 20kb
+        },
+      },
+    ],
+  },
+};
